849812b2e3051b321fb279b9651d02015e599c73
Mohamedtharwat8 <m.tharwat897@gmail.com>
2023-03-24 17:31:39 +0200

    Add project files.

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products =await _context.Products.ToListAsync();
+            return products;
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            return products;
+
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,36 @@
+using API.Data;
+using Core.interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+///dotnet ef migrations add initialCreate -o Data/Migrations
+
+builder.Services.AddScoped<IProductRepository , ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
